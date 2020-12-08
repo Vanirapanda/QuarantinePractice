4774214bfd5e43363ec61b0aec89f5717d99c5cd
Maika <maika.r@prhsrobotics.com>
2020-12-08 16:36:17 -0500

    2

--- QuarantinePractice/src/main/java/frc/robot/RobotContainer.java
+++ QuarantinePractice/src/main/java/frc/robot/RobotContainer.java
@@ -2,22 +2,25 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.XboxController.Button;
-import edu.wpi.first.wpilibj.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.commands.PunchOut;
 import frc.robot.subsystems.PneumaticSubsystem;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 public class RobotContainer {
 
+    public static PneumaticSubsystem pneumaticSubsystem = new PneumaticSubsystem();
+
     public static XboxController controller = new XboxController(0);
 
     public RobotContainer() {
-
+        configureButtonBindings();
     }
 
     private void configureButtonBindings() {
         new JoystickButton(controller, Button.kY.value).whenPressed{
-            new PunchOut();
-        }
+            new PunchOut()
+        };
     }
 }
